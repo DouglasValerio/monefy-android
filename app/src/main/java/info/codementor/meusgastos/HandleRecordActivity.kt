f5063f53cf2f137b1858be5b34b5d8ede328c245
Douglas Valério <dglvalerio@gmail.com>
2024-08-29 19:18:06 -0300

    feat: add delete and edit options

--- app/src/main/java/info/codementor/meusgastos/HandleRecordActivity.kt
+++ app/src/main/java/info/codementor/meusgastos/HandleRecordActivity.kt
@@ -1,44 +1,148 @@
 package info.codementor.meusgastos
 
+import android.R
+import android.app.DatePickerDialog
 import android.os.Bundle
 import android.view.MenuItem
-import androidx.activity.enableEdgeToEdge
+import android.widget.ArrayAdapter
+import android.widget.DatePicker
+import android.widget.Spinner
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import info.codementor.meusgastos.database.DatabaseHandler
 import info.codementor.meusgastos.databinding.HandleRecordBinding
 import info.codementor.meusgastos.entity.FinancialRecord
-import java.time.Instant
+import info.codementor.meusgastos.entity.FinancialRecordType
+import java.time.LocalDate
+import java.time.ZoneId
+import java.time.ZonedDateTime
+import java.time.format.DateTimeFormatter
+import java.util.Calendar
 
 class HandleRecordActivity : AppCompatActivity() {
     private lateinit var binding: HandleRecordBinding
 
-    private  lateinit var banco: DatabaseHandler
+    private lateinit var banco: DatabaseHandler
+    private var title: String = "Novo Registro"
+    private var isEditMode: Boolean = false
+    private var recordForEdition: FinancialRecord? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = HandleRecordBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        handleToolbar()
+
+        banco = DatabaseHandler(this)
+        handleEditMode()
+
+        handleClickListeners()
+
+        handleSpinner()
+    }
+
+    private fun handleEditMode() {
+        val id = intent.getIntExtra("item_id", -1)
+        recordForEdition = banco.find(id)
+        if (recordForEdition != null) {
+            isEditMode = true
+            title = "Editar Registro"
+            binding.dateTextField.setText(recordForEdition!!.date.toString())
+            binding.amount.setText(recordForEdition!!.amount.toString())
+            binding.description.setText(recordForEdition!!.description)
+            binding.typeSpinner.setSelection(if (recordForEdition!!.type == FinancialRecordType.EXPENSE) 0 else 1)
+            binding.btnSave.text = "Atualizar"
+
+            val formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy")
+            val zonedDateTime =
+                ZonedDateTime.ofInstant(recordForEdition!!.date, ZoneId.systemDefault())
+            zonedDateTime.format(formatter)
+            binding.dateTextField.setText(zonedDateTime.format(formatter))
+        }
+    }
+
+    private fun handleToolbar() {
         val toolbar: Toolbar = binding.toolbar
         setSupportActionBar(toolbar)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         supportActionBar?.setDisplayShowHomeEnabled(true)
 
-        supportActionBar?.title = "Novo Registro"
-        banco = DatabaseHandler(this)
+        supportActionBar?.title = title
+    }
 
+    private fun handleClickListeners() {
         binding.btnSave.setOnClickListener {
             onSaveAction()
         }
+
+        binding.calendarField.setOnClickListener() {
+            onCalendarFieldClick()
+        }
+    }
+
+    private fun handleSpinner() {
+        val spinner: Spinner = binding.typeSpinner
+
+        val options = arrayOf("Débito", "Crédito")
+
+        val adapter = ArrayAdapter(this, R.layout.simple_spinner_item, options)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+
+        spinner.adapter = adapter
+    }
+
+    private fun onCalendarFieldClick() {
+
+        // Get the current date
+        val calendar = Calendar.getInstance()
+        val year = calendar.get(Calendar.YEAR)
+        val month = calendar.get(Calendar.MONTH)
+        val day = calendar.get(Calendar.DAY_OF_MONTH)
+
+        // Show DatePickerDialog
+        val dateButton = binding.dateTextField
+        DatePickerDialog(
+            this,
+            { _: DatePicker, selectedYear: Int, selectedMonth: Int, selectedDay: Int ->
+                val selectedDate = "${selectedDay.toString().padStart(2, '0')}/${
+                    (selectedMonth + 1).toString().padStart(2, '0')
+                }/$selectedYear"
+                dateButton.setText("$selectedDate")
+            },
+            year,
+            month,
+            day
+        ).show()
+
     }
 
     private fun onSaveAction() {
-        val amount = binding.amount.text.toString().toDoubleOrNull()?:0.0
+        val amount = binding.amount.text.toString().toDoubleOrNull() ?: 0.0
         val description = binding.description.text.toString()
-        val record = FinancialRecord.createExpense(amount, description, Instant.now())
-        banco.insert (record)
-       finish()
+        val dateStr = binding.dateTextField.text.toString()
+        val type = binding.typeSpinner.selectedItem.toString()
+        val enumType =
+            if (type == "Débito") FinancialRecordType.EXPENSE else FinancialRecordType.INCOME
+
+
+        val formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy")
+
+        val localDate = LocalDate.parse(dateStr, formatter)
+
+        val date = localDate.atStartOfDay(ZoneId.systemDefault()).toInstant()
+
+        if (recordForEdition != null) {
+            val record = recordForEdition!!.update(amount, description, date, enumType)
+            banco.update(record)
+        } else {
+            val record = if (enumType == FinancialRecordType.EXPENSE) FinancialRecord.createExpense(
+                amount,
+                description,
+                date
+            ) else FinancialRecord.createIncome(amount, description, date)
+            banco.insert(record)
+        }
+        finish()
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
--- app/src/main/java/info/codementor/meusgastos/ListFragment.kt
+++ app/src/main/java/info/codementor/meusgastos/ListFragment.kt
@@ -1,18 +1,23 @@
 package info.codementor.meusgastos
 
 import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.ImageButton
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import info.codementor.meusgastos.database.DatabaseHandler
 import info.codementor.meusgastos.databinding.ActivityListBinding
 import info.codementor.meusgastos.entity.FinancialRecord
 import info.codementor.meusgastos.entity.FinancialRecordType
-import java.time.Instant
+import java.time.ZoneId
+import java.time.ZonedDateTime
+import java.time.format.DateTimeFormatter
 
 class ListFragment : Fragment() {
     private lateinit var binding: ActivityListBinding
@@ -37,41 +42,84 @@ class ListFragment : Fragment() {
 
     private fun refreshData() {
         val context = requireContext()
-       if(!::banco.isInitialized){
-           banco = DatabaseHandler(context)
-       }
-        var result = banco.findAll()
-        if (result.isEmpty()) {
-            result = mutableListOf(
-                FinancialRecord.createIncome(2000.0, "Salário", Instant.now()),
-                FinancialRecord.createIncome(1500.0, "Freelance", Instant.now())
-            )
+        if (!::banco.isInitialized) {
+            banco = DatabaseHandler(context)
         }
+        var result = banco.findAll()
+        val debts = result.filter { it.type == FinancialRecordType.EXPENSE }.sumOf { it.amount }
+        val credits = result.filter { it.type == FinancialRecordType.INCOME }.sumOf { it.amount }
+        val balance = credits - debts
+        binding.totalBalanceAmount.text = "R$$balance"
+        binding.incomeAmount.text = "R$$credits"
+        binding.expensesAmount.text = "R$$debts"
 
-        val adapter = CustomAdapter(context, result)
+        val adapter = CustomAdapter(
+            context = context,
+            items = result,
+            callback = { id -> deleteItem(id) },
+            editCallback = { id -> editItem(id) })
         binding.mainListView.adapter = adapter
     }
 
+    private fun deleteItem(id: Int) {
+        banco.delete(id)
+        refreshData()
+    }
 
-}
+    private fun editItem(id: Int) {
+        val intent = Intent(context, HandleRecordActivity::class.java).apply {
+            putExtra("item_id", id)
+        }
+        startActivity(intent)
 
-class CustomAdapter(context: Context, items: List<FinancialRecord>) :
-    ArrayAdapter<FinancialRecord>(context, R.layout.activity_list_item, items) {
+    }
 
-    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
-        val view = convertView ?: LayoutInflater.from(context).inflate(
-            R.layout.activity_list_item, parent, false
-        )
+    class CustomAdapter(
+        context: Context,
+        items: List<FinancialRecord>,
+        val callback: (Int) -> Unit,
+        val editCallback: (Int) -> Unit
+    ) :
+        ArrayAdapter<FinancialRecord>(context, R.layout.activity_list_item, items) {
 
-        val currentItem = getItem(position)
+        override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+            val view = convertView ?: LayoutInflater.from(context).inflate(
+                R.layout.activity_list_item, parent, false
+            )
 
-        val text1 = view.findViewById<TextView>(R.id.listTitle)
-        val text2 = view.findViewById<TextView>(R.id.listSubtitle)
+            val currentItem = getItem(position)
 
-        text1.text = currentItem?.description
-        val sign = if (currentItem?.type == FinancialRecordType.INCOME) "+" else "-"
-        text2.text = "$sign R\$${currentItem?.amount.toString()}"
+            val text1 = view.findViewById<TextView>(R.id.listTitle)
+            val text2 = view.findViewById<TextView>(R.id.listSubtitle)
+            val tvDate = view.findViewById<TextView>(R.id.tvDate)
+            val img = view.findViewById<ImageView>(R.id.imageView)
+            val deleteBtn = view.findViewById<ImageButton>(R.id.deleteButton)
+            val editBtn = view.findViewById<ImageButton>(R.id.editButton)
+            deleteBtn.setOnClickListener {
+                if (currentItem != null) {
+                    callback(currentItem._id)
+                }
+            }
+            editBtn.setOnClickListener {
+                if (currentItem != null) {
+                    editCallback(currentItem._id)
+                }
+            }
 
-        return view
+            val formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy")
+            val zonedDateTime = ZonedDateTime.ofInstant(currentItem?.date, ZoneId.systemDefault())
+
+
+            text1.text = currentItem?.description
+            tvDate.text = zonedDateTime.format(formatter)
+            val sign = if (currentItem?.type == FinancialRecordType.INCOME) "+" else "-"
+            img.setImageResource(
+                if (currentItem?.type == FinancialRecordType.INCOME) R.drawable.currency_income else R.drawable.currency_expense
+            )
+            text2.text = "$sign R\$${currentItem?.amount.toString()}"
+
+
+            return view
+        }
     }
 }
